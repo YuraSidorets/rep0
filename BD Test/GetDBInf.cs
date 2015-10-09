532184f29da851d5527fe0c21037bf26242791cb
YuraSidorets <Sirnick@ukr.net>
2015-10-09 19:37:32 +0300

    DB queries
    Added INSERT and SELECT queries.

---
+++ BD Test/GetDBInf.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace BD_Test
+{
+    static class GetDBInf
+    {
+        public static void GetDbinf()
+        {
+            DataTable dt = new DataTable();
+
+            MySqlConnectionStringBuilder mysqlCSB;
+
+            mysqlCSB = new MySqlConnectionStringBuilder();
+            mysqlCSB.Server = "localhost";
+            mysqlCSB.Database = "fbd";
+            mysqlCSB.UserID = "root";
+            mysqlCSB.Password = "1111";
+
+            string queryString = @"INSERT INTO table1  VALUES (2)";
+
+            using (MySqlConnection con = new MySqlConnection())
+            {
+                con.ConnectionString = mysqlCSB.ConnectionString;
+                MySqlCommand com = new MySqlCommand(queryString, con);
+                try
+                {
+                    con.Open();
+                    com.CommandText = queryString;
+                    //com.Parameters.Add("idtable1", MySqlDbType.);
+                    //com.Parameters["idtable1"].Value = 1;
+                    com.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
+            }
+        }
+        public static void GetData()
+        {
+            DataTable dt = new DataTable();
+
+
+            MySqlConnectionStringBuilder mysqlCSB;
+
+            mysqlCSB = new MySqlConnectionStringBuilder();
+            mysqlCSB.Server = "localhost";
+            mysqlCSB.Database = "fbd";
+            mysqlCSB.UserID = "root";
+            mysqlCSB.Password = "1111";
+
+            string queryString = @"SELECT idtable1 FROM table1";
+
+            using (MySqlConnection con = new MySqlConnection())
+            {
+                con.ConnectionString = mysqlCSB.ConnectionString;
+                MySqlCommand com = new MySqlCommand(queryString, con);
+                try
+                {
+                    con.Open();
+                    com.CommandText = queryString;
+                    MySqlDataReader reader = com.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("{0}\t", reader.GetInt32(0));
+                        }
+                    }
+
+                    Console.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
+            }
+
+
+        }
+    }
+}
+
---
+++ BD Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetDBInf.GetData();
+          
+        }
+    }
+}
