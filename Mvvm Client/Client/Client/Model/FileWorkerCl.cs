0d2e189b6c9a029dea563949af078d0000f2ca18
YuraSidorets <Sirnick@ukr.net>
2015-11-25 13:31:59 +0200

    done up/download functions in client

--- Mvvm Client/Client/Client/Model/FileWorkerCl.cs
+++ Mvvm Client/Client/Client/Model/FileWorkerCl.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
-namespace Client
+namespace Client                                                                                                                     
 {
     /// <summary>
      /// Gets information about file
@@ -22,6 +19,7 @@ namespace Client
             fileInformation.Add("Type", fileType);
 
             DateTime fileCreationDate = File.GetCreationTime(path);
+
             fileInformation.Add("Date", fileCreationDate);
 
             FileInfo fi = new FileInfo(path);
@@ -44,25 +42,6 @@ namespace Client
                 Console.WriteLine("this file does not exist\n method will return null\n", e);
             }
             return output;
-        }
-
-        /// <summary>
-        /// Gets file from db and creates it in pathOfCreatedFile
-        /// </summary>
-        /// <param name="id">Id of the file</param>
-        /// <param name="pathOfCreatedFile">Directory of file to create</param>
-        public static void GetFileFromDB(int id, string pathOfCreatedFile)
-        {
-            object[] arr = null;//DBWorker.GetFileToWrite(id);
-            string name = arr[0].ToString();
-            string type = arr[1].ToString();
-            byte[] bytes = arr[2] as byte[];
-            string path = string.Format("{0}{1}{2}", pathOfCreatedFile, name, type);
-            FileStream fs = File.Create(path);
-            BinaryWriter bwr = new BinaryWriter(fs);
-            bwr.Write(bytes);
-            bwr.Close();
-            Console.WriteLine("file was created");
         }
     }
 }
